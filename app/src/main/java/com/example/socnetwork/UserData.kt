7acf2659d344a5a79fd29d2a4ef88bf7bf4d28dd
oleksandreremenko25 <oleksandreremenko25@gmail.com>
2021-12-02 21:16:38 +0200

    change LinearLayout to the ConstraintLayout

--- app/src/main/java/com/example/socnetwork/UserData.kt
+++ app/src/main/java/com/example/socnetwork/UserData.kt
@@ -3,11 +3,11 @@ package com.example.socnetwork
 class UserData() {
      var usersList = listOf(
         User("Sasha", "sasha@gmail.com", "нічого", "yesterday", "https://pbs.twimg.com/profile_images/2881220369/2b27ac38b43b17a8c5eacfc443ce3384_400x400.jpeg", "Почётный доктор около 20 ведущих университетов мира, член многих Академий наук", 245, 1290, 46, 162),
-        User("Inna", "inna@gmail.com", "читання книги", "two day ago", "https://cdnimg.rg.ru/img/content/177/24/41/1_d_850.jpg", "", 245, 1290, 46, 162),
-        User("Oleg", "oleg@gmail.com", "вивчання психології", "five minutes ago", "https://vufku.org/wp-content/uploads/2019/04/Tobilevych_Sadovsky_M_K.jpg", "", 245, 1290, 46, 162),
-        User("Andriy", "andriy@gmail.com", "прогулянки з друзями", "six minutes ago", "https://upload.wikimedia.org/wikipedia/commons/7/78/Andriy_Shevchenko_2017.jpg", "", 245, 1290, 46, 162),
-        User("Ivan", "ivan@gmail.com", "прогулянки лісом", "five minutes ago", "https://isroe.co.il/wp-content/uploads/2018/09/74021.jpg", "", 245, 1290, 46, 162),
-        User("Vasil", "vasil@gmail.com", "садівництво", "five minutes ago", "https://ichef.bbci.co.uk/news/800/cpsprodpb/1946/production/_116907460_149373078_10160682506037598_2399164488474705570_n.jpg", "", 245, 1290, 46, 162),
-        User("Mykola", "mykola@gmail.com", "читання книг", "five minutes ago", "", "", 245, 1290, 46, 162)
+        User("Inna", "inna@gmail.com", "читання книги", "two day ago", "https://cdnimg.rg.ru/img/content/177/24/41/1_d_850.jpg", "NNA — румынская певица, исполнительница танцевальной музыки. Инна начала работу с Play & Win в 2008 году.", 245, 1290, 46, 162),
+        User("Oleg", "oleg@gmail.com", "вивчання психології", "five minutes ago", "https://vufku.org/wp-content/uploads/2019/04/Tobilevych_Sadovsky_M_K.jpg", "Оле́г Анато́льевич Ви́нник — украинский певец, композитор и автор песен.", 245, 1290, 46, 162),
+        User("Andriy", "andriy@gmail.com", "прогулянки з друзями", "six minutes ago", "https://upload.wikimedia.org/wikipedia/commons/7/78/Andriy_Shevchenko_2017.jpg", "Андре́й Никола́евич Шевче́нко — украинский футбольный тренер и политический деятель, ранее выступавший как футболист", 245, 1290, 46, 162),
+        User("Ivan", "ivan@gmail.com", "прогулянки лісом", "five minutes ago", "https://isroe.co.il/wp-content/uploads/2018/09/74021.jpg", "Ива́н IV Васи́льевич, прозванный Гро́зным, в постриге — Иона — государь, великий князь", 245, 1290, 46, 162),
+        User("Vasil", "vasil@gmail.com", "садівництво", "five minutes ago", "https://ichef.bbci.co.uk/news/800/cpsprodpb/1946/production/_116907460_149373078_10160682506037598_2399164488474705570_n.jpg", "Василий Ярославович Вирастюк — украинский спортсмен, заслуженный мастер спорта Украины", 245, 1290, 46, 162),
+        User("Mykola", "mykola@gmail.com", "читання книг", "five minutes ago", "", "Никола́й Гу́риевич Кули́ш (укр. Микола Гурович Куліш, 1892—1937) — украинский драматург и журналист, педагог. ", 245, 1290, 46, 162)
     )
 }
